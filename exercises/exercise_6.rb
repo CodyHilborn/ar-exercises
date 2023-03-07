19d42519a65f458ab2f190fedd06279579b299e0
Cody Hilborn <codyhilborn11@gmail.com>
2023-03-07 00:51:31 +0000

    complete exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,10 +10,10 @@ puts "----------"
 
 # Your code goes here ...
 @store1.employees.create(first_name: "Cody", last_name: "Hilborn", hourly_rate: 60)
-@store1.employees.create(first_name: "Sam", last_name: "Maisha", hourly_rate: 7)
-@store1.employees.create(first_name: "Cam", last_name: "Miller", hourly_rate: 20)
-@store2.employees.create(first_name: "Zak", last_name: "Copeland", hourly_rate: 15)
-@store2.employees.create(first_name: "Brock", last_name: "Elliott", hourly_rate: 40)
-@store2.employees.create(first_name: "Nate", last_name: "Copeland", hourly_rate: 25)
+@store1.employees.create(first_name: "Sam", last_name: "Maisha", hourly_rate: 75)
+@store1.employees.create(first_name: "Cam", last_name: "Miller", hourly_rate: 50)
+@store2.employees.create(first_name: "Zak", last_name: "Copeland", hourly_rate: 60)
+@store2.employees.create(first_name: "Brock", last_name: "Elliott", hourly_rate: 41)
+@store2.employees.create(first_name: "Nate", last_name: "Copeland", hourly_rate: 65)
 
 puts Employee.count
